c2152cd3688edcdeaa5af0e0df62c42c2fffc5db
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2023-09-06 10:14:11 +0500

    Completed hand tracking project

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -8,17 +8,20 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-pTime = 0
-cTime = 0
+pTime=0
+cTime=0
 
 while True:
-    success, img = cap.read()
+    succes, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
+    
+    # print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
-        for handLandmark in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLandmark.landmark):
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 print(id, cx, cy)
@@ -26,17 +29,13 @@ while True:
                 if id == 4:
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
-            mpDraw.draw_landmarks(img, handLandmark, mpHands.HAND_CONNECTIONS)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1/(cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f"FPS: {int(fps)}", (10, 70), cv2.FONT_HERSHEY_COMPLEX,
-                3, (255,0, 255), 3)
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
 
-    cv2.imshow('Image', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+    cv2.imshow("Image", img) 
+    cv2.waitKey(1)
